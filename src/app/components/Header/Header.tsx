99d981b87d31efc73554c1fb36a2f5154468d446
FelipeFama <lipehfama@gmail.com>
2024-06-11 14:43:54 -0300

    update settings

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
+import { MdDarkMode } from "react-icons/md";
 
 export default function Header() {
   return (
@@ -12,11 +13,25 @@ export default function Header() {
         <ul className="flex items-center ml-5">
           <li className="flex items-center">
             <Link href="/auth">
-              <FaUserCircle />
+              <FaUserCircle className="cursor-pointer" />
             </Link>
+          </li>
+          <li className="ml-2">
+            <MdDarkMode className="cursor-pointer" />
           </li>
         </ul>
       </div>
+      <ul className="flex items-center justify-between w-full md:w-1/3 mt-4">
+        <li className="hover:translate-y-2 duration-500 transition-all">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="hover:translate-y-2 duration-500 transition-all">
+          <Link href="/rooms">Rooms</Link>
+        </li>
+        <li className="hover:translate-y-2 duration-500 transition-all">
+          <Link href="/">Contact</Link>
+        </li>
+      </ul>
     </header>
   );
 }
