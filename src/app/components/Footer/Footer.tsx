09bb6df4e13f265f6fd0b8a66fdec29f9cca112e
FelipeFama <lipehfama@gmail.com>
2024-06-12 12:44:22 -0300

    feat: add new tags in footer component

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import React from "react";
+import { BiMessageDetail } from "react-icons/bi";
+import { BsFillSendFill, BsTelephoneOutbound } from "react-icons/bs";
 
 export default function Footer() {
   return (
@@ -8,6 +10,24 @@ export default function Footer() {
         <Link href="/" className="font-black text-tertiary-dark">
           Hotelzz
         </Link>
+        <h4 className="font-semibold text-[40px] py-6">Contact</h4>
+        <article className="flex flex-wrap gap-16 items-center justify-between">
+          <aside className="flex-1">
+            <p>123 Road</p>
+            <div className="flex items-center py-4">
+              <BsFillSendFill />
+              <p className="ml-2">codewithlari</p>
+            </div>
+            <div className="flex items-center">
+              <BsTelephoneOutbound />
+              <p className="ml-2">000-000-00</p>
+            </div>
+            <div className="flex items-center pt-4">
+              <BiMessageDetail />
+              <p className="ml-2">codewithlari</p>
+            </div>
+          </aside>
+        </article>
       </section>
     </footer>
   );
