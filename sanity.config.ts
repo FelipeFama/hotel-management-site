20efbe8affe168fef13c7a2b2f1424747dd0704c
FelipeFama <lipehfama@gmail.com>
2024-06-26 23:07:00 -0300

    feat: add new package and environment variables

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: "default",
   title: "hotel-management",
 
-  projectId: "x50sntpk",
-  dataset: "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
 
   basePath: "/studio",
 
