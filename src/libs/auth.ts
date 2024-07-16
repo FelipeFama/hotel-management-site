dbe7ec56bb2e57db918682e7e6160a4ac6b3ba34
FelipeFama <lipehfama@gmail.com>
2024-07-16 19:44:04 -0300

    feat: add initial settings auth

---
+++ src/libs/auth.ts
@@ -0,0 +1,25 @@
+import { NextAuthOptions } from "next-auth";
+import { SanityAdapter, SanityCredentials } from "next-auth-sanity";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    SanityCredentials(),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  adapter: SanityAdapter(),
+  debug: process.env.NODE_ENV === "development",
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {},
+};
