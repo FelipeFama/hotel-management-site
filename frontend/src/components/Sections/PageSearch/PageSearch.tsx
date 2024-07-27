00dadafb2ec137d215074f5aef998a60a0f7c948
FelipeFama <lipehfama@gmail.com>
2024-07-26 22:16:59 -0300

    feat: add search feature

--- frontend/src/components/Sections/PageSearch/PageSearch.tsx
+++ frontend/src/components/Sections/PageSearch/PageSearch.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Search } from "../Search/Search";
 
 export const PageSearch = () => {
+  const [roomTypeFilter, setRoomTypeFilter] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
   return (
     <>
-      <Search />
+      <Search
+        roomTypeFilter={roomTypeFilter}
+        searchQuery={searchQuery}
+        setRoomTypeFilter={setRoomTypeFilter}
+        setSearchQuery={setSearchQuery}
+      />
     </>
   );
 };
--- frontend/src/components/Sections/Search/Search.tsx
+++ frontend/src/components/Sections/Search/Search.tsx
@@ -1,20 +1,46 @@
-import React from "react";
+"use client";
 
-export const Search = () => {
+import { useRouter } from "next/navigation";
+import React, { ChangeEvent, FC } from "react";
+
+type Props = {
+  roomTypeFilter: string;
+  searchQuery: string;
+  setRoomTypeFilter: (value: string) => void;
+  setSearchQuery: (value: string) => void;
+};
+
+export const Search: FC<Props> = ({
+  roomTypeFilter,
+  searchQuery,
+  setRoomTypeFilter,
+  setSearchQuery,
+}) => {
+  const router = useRouter();
+
+  const handleRoomTypeChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setRoomTypeFilter(event.target.value);
+  };
+
+  const handleSearchQueryChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleFilterClick = () => {
+    router.push(`/rooms?roomType=${roomTypeFilter}&searchQuery=${searchQuery}`);
+  };
+
   return (
     <section className="rounden-lg bg-tertiary-light px-4 py-6">
       <article className="flex-wtap container mx-auto flex items-center justify-between gap-4">
         <aside className="md:1/3 mb-4 w-full md:mb-0 lg:w-auto">
-          <label
-            htmlFor=""
-            className="mb-2 block text-sm font-medium text-black"
-          >
+          <label className="mb-2 block text-sm font-medium text-black">
             Room Type
           </label>
           <div className="relative">
             <select
-              name=""
-              id=""
+              value={roomTypeFilter}
+              onChange={handleRoomTypeChange}
               className="w-full rounded px-4 py-2 capitalize leading-tight focus:outline-none dark:bg-black"
             >
               <option value="All">All</option>
@@ -25,10 +51,7 @@ export const Search = () => {
           </div>
         </aside>
         <aside className="md:1/3 mb-4 w-full md:mb-0 lg:w-auto">
-          <label
-            htmlFor=""
-            className="mb-2 block text-sm font-medium text-black"
-          >
+          <label className="mb-2 block text-sm font-medium text-black">
             Search
           </label>
           <input
@@ -36,9 +59,15 @@ export const Search = () => {
             id="search"
             placeholder="Search..."
             className="w-full rounded px-4 py-3 leading-tight placeholder:text-black focus:outline-none dark:bg-black dark:placeholder:text-white"
+            value={searchQuery}
+            onChange={handleSearchQueryChange}
           />
         </aside>
-        <button className="btn-primary" type="button">
+        <button
+          className="btn-primary"
+          type="button"
+          onClick={handleFilterClick}
+        >
           Search
         </button>
       </article>
