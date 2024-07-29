200776b110c0db545fb40b098aa45cad98c4821b
FelipeFama <lipehfama@gmail.com>
2024-07-29 16:03:33 -0300

    feat: add finally fetch in featuredroom

--- frontend/src/app/(web)/page.tsx
+++ frontend/src/app/(web)/page.tsx
@@ -2,8 +2,12 @@ import { Gallery } from "@/components/Sections/Gallery/Gallery";
 import { HeroSection } from "@/components/Sections/HeroSection/HeroSection";
 import { NewsLetter } from "@/components/Sections/NewsLetter/NewsLetter";
 import { PageSearch } from "@/components/Sections/PageSearch/PageSearch";
+import { getFeaturedRoom } from "@/libs/apis";
 
-export default function Home() {
+export default async function Home() {
+  const featuredRoom = await getFeaturedRoom();
+  console.log(featuredRoom);
+
   return (
     <>
       <HeroSection />
--- frontend/src/libs/apis.ts
+++ frontend/src/libs/apis.ts
@@ -1,8 +1,9 @@
+import { Room } from "@/models/room";
 import { sanityClient } from "./sanity";
 import * as queries from "./sanityQueries";
 
 export async function getFeaturedRoom() {
-  const result = await sanityClient.fetch(
+  const result = await sanityClient.fetch<Room>(
     queries.getFeaturedRoomQuery,
     {},
     { cache: "no-cache" },
