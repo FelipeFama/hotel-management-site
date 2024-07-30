d6b8367159e8048df123ce77087880e6aefc1ab1
FelipeFama <lipehfama@gmail.com>
2024-07-30 15:12:03 -0300

    feat: add loading component

---
+++ frontend/src/app/(web)/loading.tsx
@@ -0,0 +1,5 @@
+export default function LoadingSpinner() {
+  <aside className="flex h-screen items-center justify-center">
+    <div className="h-16 w-16 animate-spin rounded-full border-b-2 border-t-2 border-gray-900"></div>
+  </aside>;
+}
