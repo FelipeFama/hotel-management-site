65bb672f8a50250f83ae4aa8a054b9bafb904d33
FelipeFama <lipehfama@gmail.com>
2024-08-12 21:24:06 -0300

    feat: add new fetch in implement verifications in page roomdetails

--- frontend/src/app/(web)/rooms/[slug]/page.tsx
+++ frontend/src/app/(web)/rooms/[slug]/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+
+import { getRoom } from "@/libs/apis";
 import React from "react";
+import useSWR from "swr";
+import LoadingSpinner from "../../loading";
 
 export default function RoomDetails(props: { params: { slug: string } }) {
   const {
     params: { slug },
   } = props;
-  console.log(slug);
+
+  const fetchRoom = async () => getRoom(slug);
+
+  const { data: room, error, isLoading } = useSWR("/api/room", fetchRoom);
+  console.log(room);
+  if (error) throw new Error("Cannot fetch data");
+  if (typeof room === "undefined" && !isLoading)
+    throw new Error("Cannot fetch data");
+
+  if (!room) return <LoadingSpinner />;
+
+  console.log(room);
   return (
     <section>
       <div>RoomDetails</div>
