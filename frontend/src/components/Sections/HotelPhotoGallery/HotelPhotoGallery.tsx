e069c46e50e83f5dfa166df43c09ca691b48fd32
FelipeFama <lipehfama@gmail.com>
2024-08-13 19:16:40 -0300

    feat: add new tags in feature show other images in hotel photo gallery

--- frontend/src/components/Sections/HotelPhotoGallery/HotelPhotoGallery.tsx
+++ frontend/src/components/Sections/HotelPhotoGallery/HotelPhotoGallery.tsx
@@ -26,6 +26,11 @@ export const HotelPhotoGallery: FC<{ photos: ImageType[] }> = ({ photos }) => {
     );
   };
 
+  const maximumVisiblePhotos = 4;
+  const totalPhotos = photos.length;
+  const displayPhotos = photos.slice(1, maximumVisiblePhotos - 1);
+  const remainingPhotosCount = totalPhotos - maximumVisiblePhotos;
+
   return (
     <section className="container mx-auto">
       <article className="relative grid gap-5 px-3 md:grid-cols-2">
@@ -60,113 +65,38 @@ export const HotelPhotoGallery: FC<{ photos: ImageType[] }> = ({ photos }) => {
             {currentPhotoIndex + 1} / {photos.length}
           </span>
         </aside>
+
+        <aside className="hidden h-full grid-cols-2 gap-5 md:grid">
+          {displayPhotos.map((photo, index) => (
+            <figure
+              key={index}
+              className="h-64 cursor-pointer overflow-hidden rounded-2xl"
+            >
+              <Image
+                width={150}
+                height={150}
+                src={photo.url}
+                alt={`Room photo ${index + 2}`}
+                className="img scale-animation"
+              />
+            </figure>
+          ))}
+          {remainingPhotosCount > 0 && (
+            <figure
+              className="relative h-64 cursor-pointer overflow-hidden rounded-2xl"
+              onClick={openModal.bind(this, maximumVisiblePhotos)}
+            >
+              <Image
+                width={150}
+                height={150}
+                src={photos[maximumVisiblePhotos - 1].url}
+                alt={`Room Photo ${maximumVisiblePhotos}`}
+                className="img"
+              />
+            </figure>
+          )}
+        </aside>
       </article>
     </section>
   );
 };
-
-/*
-<section className='container mx-auto'>
-<article className='grid md:grid-cols-2 relative gap-5 px-3'>
-  <aside className='h-[540px] relative rounded-2xl overflow-hidden'>
-    <figure className='hidden md:flex justify-center items-center w-full h-full'>
-      <Image
-        src={photos[0].url}
-        alt={`Room Photo ${currenPhotoIndex + 1}`}
-        className='img scale-animation cursor-pointer'
-        width={150}
-        height={150}
-        onClick={openModal.bind(this, 0)}
-      />
-    </figure>
-    <figure className='md:hidden flex justify-center items-center w-full h-full'>
-      <Image
-        src={photos[currenPhotoIndex].url}
-        alt={`Room Photo ${currenPhotoIndex + 1}`}
-        className='img'
-        width={150}
-        height={150}
-        onClick={openModal.bind(this, 0)}
-      />
-    </figure>
-  </aside>
-  <aside className='md:hidden flex justify-between items-center'>
-    <figure className='flex space-x-2'>
-      <FaArrowLeft className='cursor-pointer' onClick={handlePrevious} />
-      <FaArrowRight className='cursor-pointer' onClick={handleNext} />
-    </figure>
-    <span>
-      {currenPhotoIndex + 1} / {photos.length}
-    </span>
-  </aside>
-
-  <div className='hidden md:grid grid-cols-2 h-full gap-5'>
-    {displayPhotos.map((photo, index) => (
-      <div
-        key={index}
-        className='cursor-pointer h-64 rounded-2xl overflow-hidden'
-      >
-        <Image
-          width={150}
-          height={150}
-          src={photo.url}
-          alt={`Room Photo ${index + 2}`}
-          className='img scale-animation'
-        />
-      </div>
-    ))}
-    {remainingPhotosCount > 0 && (
-      <div
-        className='cursor-pointer relative h-64 rounded-2xl overflow-hidden'
-        onClick={openModal.bind(this, maximumVisiblePhotos)}
-      >
-        <Image
-          width={150}
-          height={150}
-          src={photos[maximumVisiblePhotos - 1].url}
-          alt={`Room Photo ${maximumVisiblePhotos}`}
-          className='img'
-        />
-        <div className='absolute cursor-pointer text-white inset-0 flex justify-center bg-[rgba(0,0,0,0.5)] items-center text-2xl'>
-          + {remainingPhotosCount}
-        </div>
-      </div>
-    )}
-  </div>
-
-  {showModal && (
-    <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-90 z-[55]'>
-      <div className='h-[75vh] w-[320px] md:w-[700px] relative'>
-        <Image
-          src={photos[currenPhotoIndex].url}
-          alt={`Room Photo ${currenPhotoIndex + 1}`}
-          width={150}
-          height={150}
-          className='img'
-        />
-        <div className='flex justify-between items-center py-3'>
-          <div className='flex space-x-2 items-center text-white'>
-            <FaArrowLeft
-              className='cursor-pointer'
-              onClick={handlePrevious}
-            />
-            <FaArrowRight
-              className='cursor-pointer'
-              onClick={handleNext}
-            />
-          </div>
-          <span className='text-white text-sm'>
-            {currenPhotoIndex + 1} / {photos.length}
-          </span>
-        </div>
-        <button
-          className='absolute top-2 right-2 text-white text-lg'
-          onClick={closeModal}
-        >
-          <MdCancel className='font-medium text-2xl text-tertiary-dark' />
-        </button>
-      </div>
-    </div>
-  )}
-</article>
-</section>*/
