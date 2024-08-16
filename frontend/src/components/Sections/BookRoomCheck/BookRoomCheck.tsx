a5446581a3eaae3690f3f0ed99d6ec0688967e7b
FelipeFama <lipehfama@gmail.com>
2024-08-16 15:45:09 -0300

    feat: add render dates in bookroomchecks component

--- frontend/src/components/Sections/BookRoomCheck/BookRoomCheck.tsx
+++ frontend/src/components/Sections/BookRoomCheck/BookRoomCheck.tsx
@@ -10,9 +10,28 @@ type Props = {
 export const BookRoomCheck: FC<Props> = props => {
   const { price, discount } = props;
 
+  const discountPrice = price - (price / 100) * discount;
+
   return (
     <article className="px-7 py-6">
-      <h3></h3>
+      <h3>
+        <span
+          className={`${discount ? "text-gray-400" : ""} text-xl font-bold`}
+        >
+          $ {price}
+        </span>
+        {discount ? (
+          <span className="text-lg font-bold">
+            {" "}
+            | discount {discount}%. Now{" "}
+            <span className="text-tertiary-dark">$ {discountPrice}</span>{" "}
+          </span>
+        ) : (
+          ""
+        )}
+      </h3>
+
+      <div className="my-2 w-full border-b-2 border-b-secondary"></div>
     </article>
   );
 };
