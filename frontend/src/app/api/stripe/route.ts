a2372e8065ed06156fd0f19866dac369a8a36016
FelipeFama <lipehfama@gmail.com>
2024-08-22 11:27:20 -0300

    feat: add origin header and the session authentication check

--- frontend/src/app/api/stripe/route.ts
+++ frontend/src/app/api/stripe/route.ts
@@ -1,3 +1,5 @@
+import { authOptions } from "@/libs/auth";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
@@ -33,4 +35,16 @@ export async function POST(req: Request, res: Response) {
   ) {
     return new NextResponse("Please all fields are required", { status: 400 });
   }
+
+  const origin = req.headers.get("origin");
+
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return new NextResponse("Authentication required", { status: 400 });
+  }
+
+  const userId = session.user.id;
+  const formattedCheckoutDate = checkoutDate.split("T")[0];
+  const formattedCheckinDate = checkinDate.split("T")[0];
 }
