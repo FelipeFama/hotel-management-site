54de059b5f02e3c891dcc226a9ca4b9c5a2c9dd8
FelipeFama <lipehfama@gmail.com>
2024-08-22 10:33:48 -0300

    feat: add implement POST request validation for bookings

--- frontend/src/app/api/stripe/route.ts
+++ frontend/src/app/api/stripe/route.ts
@@ -1,5 +1,36 @@
+import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2024-06-20",
 });
+
+type RequestData = {
+  checkinDate: string;
+  checkoutDate: string;
+  adults: number;
+  children: number;
+  numberOfDays: number;
+  hotelRoomSlug: string;
+};
+
+export async function POST(req: Request, res: Response) {
+  const {
+    checkinDate,
+    adults,
+    checkoutDate,
+    children,
+    hotelRoomSlug,
+    numberOfDays,
+  }: RequestData = await req.json();
+
+  if (
+    !checkinDate ||
+    !checkoutDate ||
+    !adults ||
+    !hotelRoomSlug ||
+    !numberOfDays
+  ) {
+    return new NextResponse("Please all fields are required", { status: 400 });
+  }
+}
