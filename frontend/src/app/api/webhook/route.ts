ccdad8032d4f9e2c66dfc91fdbfaa2ec51e79ef8
FelipeFama <lipehfama@gmail.com>
2024-08-23 12:08:23 -0300

    feat: add POST request validation webhook stripe

--- frontend/src/app/api/webhook/route.ts
+++ frontend/src/app/api/webhook/route.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const checkout_session_completed = "checkout.session.completed";
@@ -5,3 +6,18 @@ const checkout_session_completed = "checkout.session.completed";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2024-06-20",
 });
+
+export async function POST(req: Request, res: Response) {
+  const reqBody = await req.text();
+  const sig = req.headers.get("stripe-signature");
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+  let event: Stripe.Event;
+
+  try {
+    if (!sig || !webhookSecret) return;
+    event = stripe.webhooks.constructEvent(reqBody, sig, webhookSecret);
+  } catch (error: any) {
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 500 });
+  }
+}
