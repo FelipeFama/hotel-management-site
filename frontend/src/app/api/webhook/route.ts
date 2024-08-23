cb6b487b70f4795505955e0fdfc13cf8f04b5e7a
FelipeFama <lipehfama@gmail.com>
2024-08-23 10:08:44 -0300

    feat: add webhook route in api folder

---
+++ frontend/src/app/api/webhook/route.ts
@@ -0,0 +1,7 @@
+import Stripe from "stripe";
+
+const checkout_session_completed = "checkout.session.completed";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2024-06-20",
+});
