d1b92ad11abfbe09b40af821d454314e5a0561c8
FelipeFama <lipehfama@gmail.com>
2024-08-24 16:16:50 -0300

    feat: add fetch in user page

--- frontend/src/app/(web)/users/[id]/page.tsx
+++ frontend/src/app/(web)/users/[id]/page.tsx
@@ -1,4 +1,38 @@
+"use client";
+
+import { getUserBookings } from "@/libs/apis";
+import axios from "axios";
+import useSWR from "swr";
+
 export default function UserDetails(props: { params: { id: string } }) {
+  const {
+    params: { id: userId },
+  } = props;
+
+  const fetchUserBooking = async () => getUserBookings(userId);
+  const fetchUserData = async () => {
+    const { data } = await axios.get("/api/users");
+    return data;
+  };
+
+  const {
+    data: userBookings,
+    error,
+    isLoading,
+  } = useSWR("/api/userBooking", fetchUserBooking);
+
+  const {
+    data: userData,
+    isLoading: loadingUserData,
+    error: errorGettingUserData,
+  } = useSWR("/api/users", fetchUserData);
+
+  if (error || errorGettingUserData) throw new Error("Cannot fetch data");
+  if (typeof userBookings === "undefined" && !isLoading)
+    throw new Error("Cannot fetch data");
+
+  console.log(userBookings);
+
   return (
     <section>
       <div>user details</div>
