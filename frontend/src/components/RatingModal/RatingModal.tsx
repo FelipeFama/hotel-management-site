c59a103544947e30b142b1d2967f2936f7e40843
FelipeFama <lipehfama@gmail.com>
2024-08-24 19:53:18 -0300

    feat: add rating modal component

---
+++ frontend/src/components/RatingModal/RatingModal.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+
+type Props = {
+  isOpen: boolean;
+};
+
+export const RatingModal: FC<Props> = props => {
+  const { isOpen } = props;
+  return (
+    <section>
+      <div></div>
+    </section>
+  );
+};
