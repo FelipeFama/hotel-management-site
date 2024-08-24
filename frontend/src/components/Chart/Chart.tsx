a43ad86d5b682ab5150de2933e481955713fcc04
FelipeFama <lipehfama@gmail.com>
2024-08-24 19:15:17 -0300

    feat: add new package

--- frontend/src/components/Chart/Chart.tsx
+++ frontend/src/components/Chart/Chart.tsx
@@ -1,4 +1,14 @@
+"use client";
+
+import {
+  Chart as ChartJs,
+  Tooltip,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+} from "chart.js";
 import { FC } from "react";
+import { Bar } from "react-chartjs-2";
 
 export const Chart: FC = () => {
   return (
