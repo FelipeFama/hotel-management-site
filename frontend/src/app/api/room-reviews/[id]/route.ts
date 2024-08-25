45cfce8a2a641dfb12302729633c4d7a4d348668
FelipeFama <lipehfama@gmail.com>
2024-08-25 14:22:35 -0300

    feat: add new route api room-reviews

---
+++ frontend/src/app/api/room-reviews/[id]/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  const rooomId = params.id;
+
+  try {
+    const roomReviews = await getRoomReviews(roomId);
+
+    return NextResponse.json(roomReviews, {
+      status: 200,
+      statusText: "Succesful",
+    });
+  } catch (error) {
+    console.log("Getting Review Failed", error);
+    return new NextResponse("Unable to fetch", { status: 400 });
+  }
+}
--- frontend/src/components/Sections/RoomReview/RoomReview.tsx
+++ frontend/src/components/Sections/RoomReview/RoomReview.tsx
@@ -1,11 +1,24 @@
 import { Review } from "@/models/review";
 import axios from "axios";
 import { FC } from "react";
+import useSWR from "swr";
 
 export const RoomReview: FC<{ roomId: string }> = ({ roomId }) => {
   const fetchRoomReviews = async () => {
     const { data } = await axios.get<Review[]>(`/api/room-reviews/${roomId}`);
     return data;
   };
+
+  const {
+    data: roomReviews,
+    error,
+    isLoading,
+  } = useSWR("/api/room-reviews", fetchRoomReviews);
+
+  if (error) throw new Error("Cannot fetch data");
+  if (typeof roomReviews === "undefined" && !isLoading)
+    throw new Error("Cannot fetch data");
+
+  console.log(roomReviews);
   return <article></article>;
 };
