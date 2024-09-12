fd19e7dd6fcf63bc479a5f57cf281cfd861656d0
FelipeFama <lipehfama@gmail.com>
2024-09-11 21:22:48 -0300

    update icons project

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
           crossOrigin="anonymous"
         />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={poppins.className}>
         <NextAuthProvider>
